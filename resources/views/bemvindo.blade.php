94e5467544f78ea42dfdaf4b96952684d0a52e0a
DevX <devx@gmail.com>
2024-10-09 20:42:09 -0300

    FEito a tela de bemvindo e merge feito por min a pedido do TechLead

---
+++ resources/views/bemvindo.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Projeto Git Hub</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+    <div class="container vh-100 d-flex justify-content-center align-items-center">
+        <div class="text-center">
+            <h1 class="display-4">Bem-vindo(a)!</h1>
+            <p class="lead">Você está cadastro no nosso sistema, obrigado pela escolhe. Aproveite nosso sistema!!</p>
+            <a href="" class="btn btn-outline-danger btn-lg mt-3">Logout</a>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
